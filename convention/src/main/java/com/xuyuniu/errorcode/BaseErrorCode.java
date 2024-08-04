8d6f2eab3da152c27dcda53313dd257090e7008f
fanghao <hashfangh000@163.com>
2024-08-04 22:52:28 +0800

    add execption

---
+++ convention/src/main/java/com/xuyuniu/errorcode/BaseErrorCode.java
@@ -0,0 +1,38 @@
+package com.xuyuniu.errorcode;
+
+/**
+ * @author: xuyuniu
+ **/
+public enum BaseErrorCode implements IErrorCode{
+
+    // ========== 一级宏观错误码 客户端错误 ==========
+    CLIENT_ERROR("A000001", "用户端错误"),
+
+
+    // ========== 二级宏观错误码 系统请求缺少幂等Token ==========
+    IDEMPOTENT_TOKEN_NULL_ERROR("A000200", "幂等Token为空"),
+    SERVICE_ERROR("B000001", "系统执行出错"),
+    // ========== 二级宏观错误码 系统执行超时 ==========
+    SERVICE_TIMEOUT_ERROR("B000100", "系统执行超时"),
+
+    // ========== 一级宏观错误码 调用第三方服务出错 ==========
+    REMOTE_ERROR("C000001", "调用第三方服务出错");
+    private final String code;
+
+    private final String message;
+
+    BaseErrorCode(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
---
+++ convention/src/main/java/com/xuyuniu/errorcode/IErrorCode.java
@@ -0,0 +1,17 @@
+package com.xuyuniu.errorcode;
+
+/**
+ * @author: xuyuniu
+ **/
+public interface IErrorCode {
+
+    /**
+     * 错误码
+     */
+    String code();
+
+    /**
+     * 错误信息
+     */
+    String message();
+}
